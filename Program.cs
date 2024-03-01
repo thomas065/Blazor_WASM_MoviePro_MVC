78b7023a39ae5a184467fbeec2b7d08555590ce4
Thomas Bell <thomasjbell065@gmail.com>
2024-02-29 21:20:57 -0500

    tested Api response, getting data

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MoviePro.Components;
+using MoviePro.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<TMDBClient>();
 
 await builder.Build().RunAsync();
---
+++ Services/TMDBClient.cs
@@ -0,0 +1,26 @@
+﻿using MoviePro.Models;
+using System.Net.Http.Json;
+
+namespace MoviePro.Services
+{
+	public class TMDBClient
+	{
+		private readonly HttpClient _httpClient;
+
+		public TMDBClient(HttpClient httpClient, IConfiguration config)
+		{
+			_httpClient = httpClient;
+
+			_httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/"); // the base url entry point for TMDB
+			_httpClient.DefaultRequestHeaders.Accept.Add(new("application/json")); // hey, i'm an application and not a user looking for json in return
+
+			string apiKey = config["TMDBKey"] ?? throw new Exception("TMDBKey not found!!");
+			_httpClient.DefaultRequestHeaders.Authorization = new("Bearer", apiKey);
+		}
+		public Task<PopularMoviePagedResponse?> GetPopularMoviesAsync()
+		{
+			return _httpClient.GetFromJsonAsync<PopularMoviePagedResponse>("movie/popular");
+		}
+	}
+
+}
